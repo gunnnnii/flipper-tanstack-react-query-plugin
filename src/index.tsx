cb07af7aaf6fcdd4c0827c635cc78febfb167e15
Gunnar Ingi Stefánsson <gunnaringi3@gmail.com>
2022-09-15 17:31:36 +0200

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,56 @@
+import React, { useRef } from 'react';
+import {PluginClient, usePlugin, createState, useValue, Layout} from 'flipper-plugin';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools'
+type Data = {
+  id: string;
+  message?: string;
+};
+
+type Events = {
+  newData: Data;
+};
+
+const queryClient = new QueryClient();
+
+// Read more: https://fbflipper.com/docs/tutorial/js-custom#creating-a-first-plugin
+// API: https://fbflipper.com/docs/extending/flipper-plugin#pluginclient
+export const plugin = (client: PluginClient<Events, {}>) => {
+  const data = createState<Record<string, Data>>({}, {persist: 'data'});
+
+  client.onMessage('newData', (newData) => {
+    data.update((draft) => {
+      draft[newData.id] = newData;
+    });
+  });
+
+  client.addMenuEntry({
+    action: 'clear',
+    handler: async () => {
+      data.set({});
+    },
+    accelerator: 'ctrl+l',
+  });
+
+  return {data};
+}
+
+// Read more: https://fbflipper.com/docs/tutorial/js-custom#building-a-user-interface-for-the-plugin
+// API: https://fbflipper.com/docs/extending/flipper-plugin#react-hooks
+export function Component() {
+  const instance = usePlugin(plugin);
+  const data = useValue(instance.data);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout.ScrollContainer>
+        {Object.entries(data).map(([id, d]) => (
+          <pre key={id} data-testid={id}>
+            {JSON.stringify(d)}
+          </pre>
+        ))}
+      </Layout.ScrollContainer>
+      <ReactQueryDevtoolsPanel setIsOpen={(o) => {}} handleDragStart={() => {}} />
+    </QueryClientProvider>
+  );
+}
